d74e9c54287d53e5530999360aca1de97db5958b
ankitgupta1999 <120298083+ankitgupta1999@users.noreply.github.com>
2023-08-02 11:22:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,63 +1,117 @@
+// import React, { useRef, useState } from 'react'
+// import '../styles/App.css';
+// const App = () => {
+//   const startTime = useRef(0);
+//   const intervalRef = useRef(0);
+//   const [currentTime, setCurrentTime] = useState(0);
+//   const [laps, setLaps] = useState([]);
+
+//   return (
+//     <div id="main">
+//       <section>
+//         <h1 className='seconds-elapsed'>Stopwatch Time</h1>
+//         <section className='buttons'>
+//           <button className="start-btn">START</button>
+//           <button className="stop-btn">STOP</button>
+//           <button className="lap-btn">LAP</button>
+//           <button className="reset-btn">RESET</button>
+//         </section>
+//       </section>
+//       <section className='lap-section'>
+//         <h2>Laps</h2>
+//         <section className='laps'>
+//           <p>lap</p>
+//           <p>lap</p>
+//           <p>lap</p>
+//         </section>
+//       </section>
+//     </div>
+//   )
+// }
+
+
+// export default App;
+
+import React, { useRef, useState, useEffect } from 'react';
+import '../styles/App.css';
+
 const App = () => {
   const startTime = useRef(0);
   const intervalRef = useRef(0);
   const [currentTime, setCurrentTime] = useState(0);
   const [laps, setLaps] = useState([]);
+  const [isLapSectionVisible, setIsLapSectionVisible] = useState(false);
 
-  const startTimer = () => {
+  useEffect(() => {
+    startTime.current = Date.now();
+
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, []);
+
+  useEffect(() => {
+    const updateTime = () => {
+      setCurrentTime((Date.now() - startTime.current) / 1000);
+    };
+
+    intervalRef.current = setInterval(updateTime, 10);
+
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, []);
+
+  const handleStart = () => {
     if (intervalRef.current !== 0) {
-      return;
+      return; // Timer is already running, ignore the click
     }
 
-    startTime.current = Date.now() - currentTime;
+    startTime.current = Date.now() - currentTime * 1000;
+
     intervalRef.current = setInterval(() => {
-      setCurrentTime(Date.now() - startTime.current);
+      setCurrentTime((Date.now() - startTime.current) / 1000);
     }, 10);
   };
 
-  const stopTimer = () => {
+  const handleStop = () => {
     clearInterval(intervalRef.current);
     intervalRef.current = 0;
   };
 
-  const addLap = () => {
-    const lapTime = formatTime(currentTime);
+  const handleLap = () => {
+    const lapTime = currentTime.toFixed(3);
     setLaps((prevLaps) => [...prevLaps, lapTime]);
+    setIsLapSectionVisible(true);
   };
 
-  const resetTimer = () => {
-    clearInterval(intervalRef.current);
-    intervalRef.current = 0;
+  const handleReset = () => {
     setCurrentTime(0);
     setLaps([]);
-  };
-
-  const formatTime = (time) => {
-    const seconds = Math.floor(time / 1000);
-    const milliseconds = Math.floor((time % 1000) / 10);
-    return `${seconds}.${milliseconds.toString().padStart(2, '0')}`;
+    setIsLapSectionVisible(false);
+    startTime.current = Date.now();
   };
 
   return (
     <div id="main">
       <section>
-        <h1 className="seconds-elapsed">{formatTime(currentTime)}</h1>
+        <h1 className="seconds-elapsed">{currentTime.toFixed(3)}</h1>
         <section className="buttons">
-          <button className="start-btn" onClick={startTimer}>
+          <button className="start-btn" onClick={handleStart}>
             START
           </button>
-          <button className="stop-btn" onClick={stopTimer}>
+          <button className="stop-btn" onClick={handleStop}>
             STOP
           </button>
-          <button className="lap-btn" onClick={addLap}>
+          <button className="lap-btn" onClick={handleLap}>
             LAP
           </button>
-          <button className="reset-btn" onClick={resetTimer}>
+          <button className="reset-btn" onClick={handleReset}>
             RESET
           </button>
         </section>
       </section>
-      {laps.length > 0 && (
+      {isLapSectionVisible && (
         <section className="lap-section">
           <h2>Laps</h2>
           <section className="laps">
@@ -70,3 +124,5 @@ const App = () => {
     </div>
   );
 };
+
+export default App;
